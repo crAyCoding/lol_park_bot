a468500b60299080148f3d53832edada3ba86c5a
crAy <craycoding@gmail.com>
2025-01-06 17:28:01 +0900

    칼바람 내전 : 오타 수정

--- src/normal_game.py
+++ src/normal_game.py
@@ -538,7 +538,7 @@ async def finalize_team(ctx, teams, board_message, summoners, host, is_aram=Fals
             # 맞는 음성 채널로 이동
             await move_summoners(ctx, teams)
             # 기록 보드 자동 출력
-            await add_normal_game_to_database(ctx, summoners, teams)
+            await add_normal_game_to_database(ctx, summoners, teams, is_aram)
 
     class EditButton(discord.ui.Button):
         def __init__(self):
